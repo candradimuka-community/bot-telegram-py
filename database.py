1b3b7db3ffed5696597e0098a3a678981587771c
aronei44 <meliodasmeliodas224@gmail.com>
2023-04-26 15:35:13 +0700

    fix (docker): add environment and change port

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = f"cockroachdb://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?sslmode=require"
+DATABASE_URL = f"cockroachdb://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?sslmode=require&options=--cluster={os.getenv('DB_CLUSTER')}"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
